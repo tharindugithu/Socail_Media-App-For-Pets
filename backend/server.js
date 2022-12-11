59e230b932368c9ed84aeb4e9dfd2825b10280bf
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2022-12-11 10:37:43 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app.js")
-const { connectDatabae } = require("./config/database.js")
-
-
-connectDatabae()
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
